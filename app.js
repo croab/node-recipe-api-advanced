5b727a1449b36af6a7eedc6b6601d0835a7ab6fb
Benjamin Croad <benjamincroadapple@gmail.com>
2022-10-18 13:21:19 +0100

    Fixed server.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const morgan = require('morgan');
+// const morgan = require('morgan');
+
+
+// Parse incoming data and add to req
+// app.use(express.json());
 
 // Set morgan to be used if in development
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+// }
--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 // Imports
-const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-const mongoose = require('mongoose');
+// Import app
+const app = require('./app');
 
-const app = express();
+// Create DB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-// Parse incoming data and add to req
-app.use(express.json());
-
-// Connect MongoDB
-const password = process.env.DATABASE_PASSWORD;
-const db = process.env.DATABASE.replace('<PASSWORD>', password);
-mongoose.connect(db).then(() => {
-  console.log('Successfully connected to database...');
+// Connect to mongoose
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
 });
 
-// Creating server
-const port = process.env.NODE_ENV || 3000;
+// Start server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is listening on port: ${port}`);
+  console.log(`App running on port ${port}...`);
 });
